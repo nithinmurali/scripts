fee9006780b1c08b6d5cc496f06cd7a3d905bcf0
nithin murali <imnmfotmal@gmail.com>
2015-04-10 06:14:20 +0530

    fixed bug in name.py

--- name.py
+++ name.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 
 import os
- 
+import sys
+
 folder = '.'
-i=0
+i = 0
+os.system('mkdir mvtemp')
 for file in os.listdir(folder):
-    os.rename(file, str(i) + os.path.splitext(file)[1] )    
+    # print file + "to" + str(i) + os.path.splitext(file)[1]
+    if str(file) == 'mvtemp':
+        continue
+        pass
+
+    cmd = 'mv ' + file + " mvtemp/"+ str(i) + os.path.splitext(file)[1] 
+    # print cmd
+    os.system(cmd) 
     i += 1;
+    # print i;
     pass
+os.system('mv mvtemp/* ./');
+os.system('rm -r mvtemp');
