0dab46e72d9b20885e3cd55fc69af3509cd913d9
nithin murali <imnmfotmal@gmail.com>
2015-05-01 10:51:19 +0530

    almost testing left

--- rmrec.py
+++ rmrec.py
@@ -3,31 +3,36 @@
 import os
 import sys
 import argparse
+import datetime
 
 folder = '.'
 
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-s',"--start",nargs='?',default =0,type = int)
+parser.add_argument('-r',"--recent",nargs='1', action="store_true" )
+parser.add_argument('-h',"--hour",nargs='1',default=0, type=int)
+parser.add_argument('-m',"--minutes",nargs='1',default=0, type=int)
 args = parser.parse_args()
 
-if args.start:
-    start = args.start
-else:
-    start = 0
+if args.hour:
+    hour = args.hour; 
 
-i = start 
-os.system('mkdir mvtemp')
 for file in os.listdir(folder):
     # print file + "to" + str(i) + os.path.splitext(file)[1]
-    if str(file) == 'mvtemp':
-        continue
-        pass
-
-    cmd = 'mv ' + file + " mvtemp/"+ str(i) + os.path.splitext(file)[1] 
-    # print cmd
-    os.system(cmd) 
-    i += 1;
-    # print i;
+    mt = modification_date(file)
+    ct = datetime.datetime.today()
+    dt = ct - mt
+    if args.hour:
+        hour = divmod(c.days * 86400 + c.seconds, 60*60)[0]
+        if hour < args.hour
+            cmd = 'mv ' + file  
+            os.system(cmd) 
+    elif args.minutes:
+        minutes = divmod(c.days * 86400 + c.seconds, 60)[0]
+        if minutes < args.minutes
+            cmd = 'ls'+file
+            os.system(cmd)
     pass
-os.system('mv mvtemp/* ./');
-os.system('rm -r mvtemp');
