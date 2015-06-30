f99ee96e0882a87f62e1caaab7e063245be49bbb
nithin murali <imnmfotmal@gmail.com>
2015-06-30 08:36:11 +0530

    bug fixes

--- rmrec.py
+++ rmrec.py
@@ -12,13 +12,10 @@ def modification_date(filename):
     return datetime.datetime.fromtimestamp(t)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r',"--recent",nargs='1', action="store_true" )
-parser.add_argument('-h',"--hour",nargs='1',default=0, type=int)
-parser.add_argument('-m',"--minutes",nargs='1',default=0, type=int)
+parser.add_argument('-r',"--recent", action="store_true" )
+parser.add_argument('-h',"--hour",nargs=1,default=0, type=int)
+parser.add_argument('-m',"--minutes",nargs=1,default=0, type=int)
 args = parser.parse_args()
-
-if args.hour:
-    hour = args.hour; 
 
 for file in os.listdir(folder):
     # print file + "to" + str(i) + os.path.splitext(file)[1]
@@ -27,12 +24,12 @@ for file in os.listdir(folder):
     dt = ct - mt
     if args.hour:
         hour = divmod(c.days * 86400 + c.seconds, 60*60)[0]
-        if hour < args.hour
+        if hour < args.hour:
             cmd = 'mv ' + file  
             os.system(cmd) 
     elif args.minutes:
         minutes = divmod(c.days * 86400 + c.seconds, 60)[0]
-        if minutes < args.minutes
+        if minutes < args.minutes:
             cmd = 'ls'+file
             os.system(cmd)
     pass
